a1a19743fd4e5f3f22f297c417e67bb861fa1791
robertzk <technoguyrob@gmail.com>
2017-05-19 16:12:46 -0500

    add todo.

--- models/email/enron.R
+++ models/email/enron.R
@@ -2,7 +2,6 @@
 # Inspired by [Seoyoung Kim's talk](https://github.com/robertzk/rfinance17-notes/blob/master/regtech.md)
 # Data set is at: https://www.cs.cmu.edu/~./enron/
 list(
- 
-
+  # TODO: Build analysis and email engine. (Will be done by may 21, 2017).
 )
 
