b16996972c84f95bcdc8601dee8abe55af4b2b47
robertzk <technoguyrob@gmail.com>
2017-05-19 15:41:16 -0500

    Add enron data

--- models/email/enron.R
+++ models/email/enron.R
@@ -1,7 +1,8 @@
 # The Enron Employee Email model! 
 # Inspired by [Seoyoung Kim's talk](https://github.com/robertzk/rfinance17-notes/blob/master/regtech.md)
+# Data set is at: https://www.cs.cmu.edu/~./enron/
 list(
-
+ 
 
 )
 
